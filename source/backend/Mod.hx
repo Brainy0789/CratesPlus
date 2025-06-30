3ef1d4b9ff368c5fc23692b6a32991ef758fa376
Brainy0789 <bpurdy12@icloud.com>
2025-06-30 11:24:16 -0700

    Added placeholder classes Mod and ModList

---
+++ source/backend/Mod.hx
@@ -0,0 +1,7 @@
+package backend;
+
+class Mod {
+    public function new() {
+        trace("The Mod class is not functional yet.")
+    }
+}
---
+++ source/backend/ModList.hx
@@ -0,0 +1,7 @@
+package backend;
+
+class ModList extends Mod {
+    override public function new() {
+        trace("The ModList class is not functional yet.")
+    }
+}
